44c732a71ab3d807b5e5e30835de9faea7b33c08
Matthew Ho <mattewho37@gmail.com>
2021-06-22 17:33:10 -0400

    Pushing React updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {Route, Switch, Link} from "react-router-dom"
+import Home from "./components/Home"
+import CookBook from "./components/CookBook"
+import Nav from "./components/Nav"
+import "./App.css"
+
+
 function App() {
-  return <main className="container"></main>;
+  return (
+    <main className="container-fluid  p-0">
+      <Nav/>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home/>
+        </Route>
+        <Route path="/cook_book">
+          <CookBook/>
+        </Route>
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
